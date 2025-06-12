48f336007da7e2555c185b01d568fedb24f18e9b
DELL <DELL@PS040>
2025-06-12 12:04:08 +0530

    Initial commit

---
+++ com.payapt/src/test/java/IndividualFlows/Reports_ParticipantFileFedwire.java
@@ -0,0 +1,29 @@
+package IndividualFlows;
+
+import org.testng.annotations.Test;
+
+import com.ExtentReport.ExtentReport;
+import com.pages.Reports_ParticpantFilePage;
+
+import base.BaseClass;
+
+public class Reports_ParticipantFileFedwire extends BaseClass{
+	@Test
+	public void ParticipantFile_Fedwire() throws InterruptedException
+	{
+		refresh();
+		ExtentReport.createTest("Participant File Fedwire Service");
+		
+		scrollByVisibilityOfElement(driver, PFreport.Reports);
+		visibleofele(driver, PFreport.MenuListParticipantFile, "Fednow Participant file");
+		clickelementwithname(PFreport.MenuListParticipantFile, "Navigating to Fednow Participant file");
+		
+		PFreport.FedwireParticipantfile();	
+		Thread.sleep(1000);
+		clickelementwithname(PFreport.resultsClosebtn, "Results Close btn");
+		ExtentReport.resetToParent();
+
+	}
+	
+
+}
